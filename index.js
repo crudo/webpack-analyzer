b7fa46c844a72dc20d74269070d614e2d4dd5334
Martin Cermak <martin.cermak@gooddata.com>
2016-03-06 20:39:35 +0100

    CLI

--- index.js
+++ index.js
@@ -1,66 +1,29 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
-var path = require('path');
+var program = require('commander');
 
-var colors = require('colors/safe');
-var filesize = require('filesize');
-var repeatString = require('repeat-string');
+var processFile = require('./lib/processFile').processFile;
 
-fs.readFile('stats.json', 'utf-8', function(err, fileData) {
-    if (err) throw err;
-    var jsonData = JSON.parse(fileData);
-    processFile(jsonData);
-});
+program
+    .usage('[options] <file ...>')
+    .option('-process')
+    .option('-o, --output', 'Output [stdout]', 'stdout')
+    .parse(process.argv);
 
-function processFile(jsonData) {
-    line();
-    var assetsTotal = 0;
-    var assetsByType = {};
+if (!program.args.length) {
+    program.help();
+} else {
+    var pathToFile = program.args[0];
 
-    jsonData.assets.forEach(function (item, i) {
-        var fileType = path.extname(item.name).replace('.', '');
-
-        assetsTotal += item.size;
-        item.type = fileType;
-
-        if (!assetsByType[fileType]) assetsByType[fileType] = {total: 0, items: []};
-        assetsByType[fileType].total += item.size;
-        assetsByType[fileType].items.push(item);
-    });
-
-    jsonData.assets.sort(function (a, b) {
-        return b.size - a.size;
-    }).forEach(function (item, i) {
-        var pct = round((item.size/(assetsTotal/100)));
-        var color = pct > 25 ? 'red' : (pct > 5 ? 'yellow' : 'blue');
-
-        console.log('+', colors[color](repeatString('‖', pct/2)), pct, item.type, colors[color](item.name), filesize(item.size));
-    });
-
-    line();
-
-    for (var type in assetsByType) {
-        var group = assetsByType[type];
-
-        console.log('\n\n', type, '\n');
-
-        group.items.sort(function (a, b) {
-            return b.size - a.size;
-        }).forEach(function (item, i) {
-            var pctOfType = round(item.size/(assetsByType[type].total/100));
-            var color = pctOfType > 25 ? 'red' : (pctOfType > 5 ? 'yellow' : 'blue');
-
-            console.log('+', colors[color](repeatString('‖', pctOfType/2)), pctOfType, item.type, colors[color](item.name), filesize(item.size));
-        });
+    if (!fs.existsSync(pathToFile)) {
+        console.error('File does not exist!');
+        process.exit(1);
     }
 
-    // console.log(assetsByType);
-}
-
-
-function round(num) {
-    return Math.round(num * 100) / 100;
-}
-
-function line() {
-    console.log('\n++++++++++++++++++++++++++++++++++++++++\n');
+    fs.readFile(pathToFile, 'utf-8', function(err, fileData) {
+        if (err) throw err;
+        var jsonData = JSON.parse(fileData);
+        processFile(jsonData);
+    });
 }
